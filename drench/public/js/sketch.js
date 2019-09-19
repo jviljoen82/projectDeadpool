519f8bbe69d04b6572434e232044529ccd5a0d19
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-09-19 15:10:49 +0200

    refactor es6

--- drench/public/js/sketch.js
+++ drench/public/js/sketch.js
@@ -1,23 +1,23 @@
 function make2DArray(cols, rows) {
-  var arr = new Array(cols);
-  for (var i = 0; i < arr.length; i++) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
   }
   return arr;
 }
 
-var cols;
-var grid;
-var rows;
-var w = 20;
+let cols;
+let grid;
+let rows;
+let w = 20;
 
 function setup() {
   createCanvas(520, 520);
   cols = floor(width / w - 2.5);
   rows = floor(height / w - 2.5);
   grid = make2DArray(cols, rows);
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       grid[i][j] = new Cell(i, j, w);
         }
     }
@@ -25,8 +25,8 @@ function setup() {
 function draw() {
   background(255);
 
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       grid[i][j].show();
         }
     }
