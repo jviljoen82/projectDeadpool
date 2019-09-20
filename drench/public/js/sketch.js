4064237cdd0b47ea7b662cf8ab85a6a0f5c61f5f
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-09-20 12:21:46 +0200

    added another button

--- drench/public/js/sketch.js
+++ drench/public/js/sketch.js
@@ -11,13 +11,14 @@ let grid;
 const cols = 14;
 const rows = 14;
 // let rows;
-let w = 20;
-const red = [255, 0, 0, 100, 100, 0];
-const blue = [0, 255, 0, 100, 0, 100];
-const green = [0, 0, 255, 0, 100, 100];
+const w = 20;
 const buttonSize = 40;
+const red = [255, 0, 0, 244, 175, 133];
+const green = [0, 0, 255, 208, 122, 193];
+const blue = [0, 255, 0, 63, 197, 233];
 
 const Button1 = new Clickable();
+const Button2 = new Clickable();
   
 
 function setup() {
@@ -42,16 +43,28 @@ function setup() {
   // **************************************************
   // button setup
   // button1
+  let btn1Color = color(red[0], green[0], blue[0]);
   Button1.x = 10;
   Button1.y = 305;
   Button1.height = buttonSize;
   Button1.width = buttonSize;
-  Button1.color = color(red[1], green[1], blue[1]);
+  Button1.color = btn1Color;
   Button1.cornerRadius = 10;
   Button1.strokeWeight = 2;
   Button1.stroke = '#000000';
-  Button1.textColor = color(red[1], green[1], blue[1]);
+  Button1.textColor = btn1Color;
 
+  // button2
+  let btn2Color = color(red[1], green[1], blue[1]);
+  Button2.x = 60;
+  Button2.y = 305;
+  Button2.height = buttonSize;
+  Button2.width = buttonSize;
+  Button2.color = btn2Color;
+  Button2.cornerRadius = 10;
+  Button2.strokeWeight = 2;
+  Button2.stroke = '#000000';
+  Button2.textColor = btn2Color;
 
   // ***************************************************
 }
@@ -62,6 +75,7 @@ function draw() {
   stroke('#949494');
   rect(5, 300, 280, 105);
   Button1.draw();
+  Button2.draw();
 
   // moved to setup to only draw once
   /*
@@ -85,6 +99,15 @@ Button1.onOutside = function () {
 Button1.onHover = function () {
   Button1.stroke = '#FFFFFF';
 };
+
+// button1
+Button2.onOutside = function () {
+  Button2.stroke = '#000000';
+};
+Button2.onHover = function () {
+  Button2.stroke = '#FFFFFF';
+};
+
 
 
 
