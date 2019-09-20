89a2ecfbcfdd5d0ece212f3606a96ab961590bf3
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-09-20 12:03:09 +0200

    button panel with first button

--- drench/public/js/sketch.js
+++ drench/public/js/sketch.js
@@ -12,9 +12,15 @@ const cols = 14;
 const rows = 14;
 // let rows;
 let w = 20;
+const red = [255, 0, 0, 100, 100, 0];
+const blue = [0, 255, 0, 100, 0, 100];
+const green = [0, 0, 255, 0, 100, 100];
+
+const Button1 = new Clickable(50, 50);
+  
 
 function setup() {
-  createCanvas(500, 600);
+  createCanvas(320, 410);
   // cols = floor(width / w - 2.5);
   // rows = floor(height / w - 2.5);
   grid = make2DArray(cols, rows);
@@ -31,10 +37,48 @@ function setup() {
       grid[i][j].show();
     }
   }
+
+  
+
+  // button setup
+  // button1
+  Button1.x = 10;
+  Button1.y = 305;
+  Button1.height = 40;
+  Button1.width = 40;
+  Button1.color = color(red[1], green[1], blue[1]);
+  Button1.cornerRadius = 10;
+  Button1.strokeWeight = 2;
+  Button1.stroke = '#000000';
+  Button1.textColor = color(red[1], green[1], blue[1]);
+
 }
 
+// *******************************
+// higlight button on hover
+// *******************************
+
+// button1
+Button1.onOutside = function () {
+  Button1.stroke = '#000000';
+};
+Button1.onHover = function () {
+  Button1.stroke = '#FFFFFF';
+};
+
+
+
+// ***************************************
+// button click methods to folow
+// ***************************************
+
+
 function draw() {
-  
+  // draw button panel with buttons
+  fill('#B9B7B7');
+  stroke('#949494');
+  rect(5, 300, 280, 105);
+  Button1.draw();
 
   // moved to setup to only draw once
   /*
