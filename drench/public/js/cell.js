52a2a28fa1a3703b59b5ba76b41717c8e7ecda5c
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-09-20 08:47:16 +0200

    colored grid

--- drench/public/js/cell.js
+++ drench/public/js/cell.js
@@ -1,14 +1,21 @@
 class Cell {
+  
   constructor(i, j, w) {
     this.i = i;
     this.j = j;
     this.x = i * w;
     this.y = j * w;
     this.w = w;
+    this.red = [255, 0, 0, 100, 100, 0];
+    this.blue = [0, 255, 0, 100, 0, 100];
+    this.green = [0, 0, 255, 0, 100, 100];
   }
   show() {
-    stroke(0);
-    noFill();
+    //stroke(0);
+    let colorRandomzer = Math.floor(random(6));
+    noStroke();
+    let c = color(this.red[colorRandomzer], this.green[colorRandomzer], this.blue[colorRandomzer]);
+    fill(c);
     rect(this.x, this.y, this.w, this.w);
   }
 }
--- drench/public/js/sketch.js
+++ drench/public/js/sketch.js
@@ -6,28 +6,43 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
-let cols;
+//let cols;
 let grid;
-let rows;
+const cols = 14;
+const rows = 14;
+// let rows;
 let w = 20;
 
 function setup() {
-  createCanvas(520, 520);
-  cols = floor(width / w - 2.5);
-  rows = floor(height / w - 2.5);
+  createCanvas(500, 600);
+  // cols = floor(width / w - 2.5);
+  // rows = floor(height / w - 2.5);
   grid = make2DArray(cols, rows);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       grid[i][j] = new Cell(i, j, w);
         }
-    }
-}
-function draw() {
+  }
+  
+  //draw grid once
   background(255);
-
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       grid[i][j].show();
-        }
     }
+  }
+}
+
+function draw() {
+  
+
+  // moved to setup to only draw once
+  /*
+    background(255);
+
+    for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].show();
+        }
+    } */ 
 }
